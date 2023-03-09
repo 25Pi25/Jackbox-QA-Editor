846530f35bcf69c832213c6b8bd323a762d3cd5c
25Pi25 <varunmash@outlook.com>
2023-03-09 01:42:40 -0800

    added fields worwgit add .git add .!

--- Assets/GameScripts/ContentList.cs
+++ Assets/GameScripts/ContentList.cs
@@ -6,3 +6,16 @@ public class ContentList : MonoBehaviour
 {
     
 }
+
+public class QuiplashQuestion : ContentList
+{
+    public bool familyFriendly;
+    public int id;
+    public string prompt;
+    public AudioClip promptAudio;
+
+    public bool hasJokeAudio;
+    public string[] jokeKeywords;
+    public string location;
+    public AudioClip keywordResponseAudio;
+}
